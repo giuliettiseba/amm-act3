3a235b1ed3e510c3bf327a0ebe1272b483e3d443
Sebastian Giulietti <32080740+giuliettiseba@users.noreply.github.com>
2025-11-23 04:59:50 -0600

    update RootNavigator to use explicit screen paths and add detail/category screens for libros and cafeteria

--- src/routes/RootNavigator.tsx
+++ src/routes/RootNavigator.tsx
@@ -7,29 +7,37 @@ const RootNavigator = () => {
     const {themeColors} = useTheme();
 
     return (
-        <Stack
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: themeColors.card,
-                },
-                headerTintColor: themeColors.primary,
-                headerTitleStyle: {
-                    color: themeColors.foreground,
-                    fontFamily: 'HankenGrotesk-SemiBold',
-                },
-            }}
-        >
-            <Stack.Protected guard={isLoggedIn}>
-                <Stack.Screen name="(tabs)" options={{title: "Home", headerShown: false}}/>
-                <Stack.Screen name="libros" options={{title: "Catálogo de Libros"}}/>
-                <Stack.Screen name="coworking" options={{title: "Coworking"}}/>
-                <Stack.Screen name="informacion" options={{title: "Informacion"}}/>
-                <Stack.Screen name="cafeteria" options={{title: "Cafetería"}}/>
-            </Stack.Protected>
-            <Stack.Protected guard={!isLoggedIn}>
-                <Stack.Screen name="sign-in" options={{headerShown: false}}/>
+        <>
+            <Stack
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: themeColors.card,
+                    },
+                    headerTintColor: themeColors.primary,
+                    headerTitleStyle: {
+                        color: themeColors.foreground,
+                        fontFamily: 'HankenGrotesk-SemiBold',
+                    },
+                }}
+            >
+                <Stack.Protected guard={isLoggedIn}>
+                    <Stack.Screen name="(tabs)" options={{title: "Home", headerShown: false}}/>
+                    <Stack.Screen name="libros/index" options={{title: "Catálogo de Libros"}}/>
+                    <Stack.Screen name="libros/[id]" options={{title: "Detalle del Libro"}}/>
+                    <Stack.Screen name="coworking/index" options={{title: "Coworking"}}/>
+                    <Stack.Screen name="informacion/index" options={{title: "Informacion"}}/>
+                    <Stack.Screen name="cafeteria/index" options={{title: "Cafetería"}}/>
+                    <Stack.Screen name="cafeteria/categoria/[id]" options={{title: "Categoría"}}/>
                 </Stack.Protected>
-        </Stack>
+                <Stack.Protected guard={!isLoggedIn}>
+                    <Stack.Screen name="sign-in" options={{headerShown: false}}/>
+                </Stack.Protected>
+
+
+            </Stack>
+
+
+        </>
     );
 };
 
